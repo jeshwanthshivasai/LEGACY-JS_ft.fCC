66b77a83a3365689e17e9a3720f5c05de7503553
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-27 15:44:25 +0530

    38

---
+++ Basic_JavaScript/38_wordblanks.js
@@ -0,0 +1,21 @@
+/*
+You are provided sentences with some missing words.
+You then fill in the missing pieces with words of your choice in a way that the completed sentence makes sense.
+
+Consider this sentence:
+
+It was really ____, and we ____ ourselves ____.
+This sentence has three missing pieces and we can add words of our choice to complete it.
+We can then assign the completed sentence to a variable as follows:
+
+const sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + ".";
+*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " fat " + myNoun + " in our street "+ myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
