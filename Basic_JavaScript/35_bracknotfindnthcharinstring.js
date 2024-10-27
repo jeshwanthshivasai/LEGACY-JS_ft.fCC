d8bb12f0121a165329d15b04ce9048b7621f2cd2
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-27 12:26:35 +0530

    35

---
+++ Basic_JavaScript/35_bracknotfindnthcharinstring.js
@@ -0,0 +1,18 @@
+/*
+You can also use bracket notation to get the character at other positions within a string.
+
+Remember that computers start counting at 0, so the first character is actually the zeroth character.
+
+Example:
+
+const firstName = "Ada";
+const secondLetterOfFirstName = firstName[1];
+secondLetterOfFirstName would have a value of the string d.
+
+*/
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
