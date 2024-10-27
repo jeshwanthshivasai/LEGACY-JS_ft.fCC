dd37f8f6d6c2e43e5b8b2b7179eb64d05924ecf9
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-27 12:19:20 +0530

    33

---
+++ Basic_JavaScript/33_bracketnotfind1stcharofstring.js
@@ -0,0 +1,19 @@
+/*
+Bracket notation is a way to get a character at a specific index within a string.
+
+Most modern programming languages, like JavaScript, don't start counting at 1 like humans do.
+They start at 0.
+This is referred to as Zero-based indexing.
+
+const firstName = "Charles";
+const firstLetter = firstName[0];
+
+firstLetter would have a value of the string C.
+*/
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
