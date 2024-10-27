de7f0abfb1ca96f372a0a9bd2f168cd93d0cac0a
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-27 09:53:39 +0530

    17th and a couple of changes in 9 & 10 within the sentences

--- Basic_JavaScript/9_diffbwvarandlet.js
+++ Basic_JavaScript/9_diffbwvarandlet.js
@@ -6,9 +6,10 @@ var camper = "David";
 console.log(camper);
 In the code above, the camper variable is originally declared as James, and is then overridden to be David. The console then displays the string David.
 
-In a small application, you might not run into this type of problem. But as your codebase becomes larger, you might accidentally overwrite a variable that you did not intend to. Because this behavior does not throw an error, searching for and fixing bugs becomes more difficult.
+In a small application, you might not run into this type of problem. But as your codebase becomes larger, you might accidentally overwrite a variable that you did not intend to. 
+Because this behavior does not throw an error, searching for and fixing bugs becomes more difficult.
 
-A keyword called let was introduced in ES6, a major update to JavaScript, to solve this potential issue with the var keyword. You'll learn about other ES6 features in later challenges.
+A keyword called let was introduced in ES6, a major update to JavaScript, to solve this potential issue with the var keyword.
 
 If you replace var with let in the code above, it results in an error:
 
