82eb72ad7ff938b611848acc7843c313ab2660c4
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-27 12:15:56 +0530

    32

---
+++ Basic_JavaScript/32_findlengthofstring.js
@@ -0,0 +1,14 @@
+/*
+You can find the length of a String value by writing .length after the string variable or string literal.
+
+console.log("Alan Peter".length);
+The value 10 would be displayed in the console.
+Note that the space character between "Alan" and "Peter" is also counted.
+*/
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
