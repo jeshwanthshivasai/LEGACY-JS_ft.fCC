0b3076c89f50fa40c232e804a1b64b25cbcbd7b9
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-27 15:49:36 +0530

    40

---
+++ Basic_JavaScript/40_nestonearraywithinanotherarray.js
@@ -0,0 +1,9 @@
+/*
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+*/
+
+// Only change code below this line
+const myArray = [["jess", 2], ["crapshit", "bullshit"]];
