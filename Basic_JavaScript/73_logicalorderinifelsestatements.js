c36c4abb60b9126ea46d11ef61ae39053ff5e6fe
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-31 21:05:42 +0530

    73

---
+++ Basic_JavaScript/73_logicalorderinifelsestatements.js
@@ -0,0 +1,17 @@
+/*
+Order is important in if, else if statements.
+
+The function is executed from top to bottom so you will want to be careful of what statement comes first.
+*/
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
