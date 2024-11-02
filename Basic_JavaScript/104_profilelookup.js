e6e9fc1fbe5ed98ec237027f2e6ffdc729af9e42
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-02 12:36:37 +0530

    104 with explanation

--- Basic_JavaScript/104_profilelookup.js
+++ Basic_JavaScript/104_profilelookup.js
@@ -54,3 +54,13 @@ function lookUpProfile(name, prop) {
 }
 
 lookUpProfile("Akira", "likes");
+
+/*
+--> The for loop runs, starting at the first object in the contacts list.
+--> If the firstName parameter passed into the function matches the value of the "firstName" key in the first object, the if statement passes.
+--> Then, we use .hasOwnProperty() method (checks if there’s a given property and returns a boolean) with prop as an argument.
+    If it’s true, the value of prop is returned.
+--> If the second if statement fails, No such property is returned.
+--> If the first if statement fails, the for loop continues on to the next object in the contacts list.
+--> If the firstName parameter isn’t matched by the final contacts object, the for loop exits and No such contact is returned.
+*/
