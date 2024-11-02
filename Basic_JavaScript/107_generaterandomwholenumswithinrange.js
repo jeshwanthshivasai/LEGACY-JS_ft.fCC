3dc3a70d437ac54b2acc83b2d00d130463218c97
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-02 12:55:56 +0530

    107

---
+++ Basic_JavaScript/107_generaterandomwholenumswithinrange.js
@@ -0,0 +1,15 @@
+/*
+You can generate a random whole number in the range from zero to a given number.
+You can also pick a different lower number for this range.
+
+You'll call your minimum number min and your maximum number max.
+
+This formula gives a random whole number in the range from min to max.
+
+--> Take a moment to read it and try to understand what this code is doing:
+    Math.floor(Math.random() * (max - min + 1)) + min
+*/
+
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
