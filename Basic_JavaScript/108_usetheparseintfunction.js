3a025887e008930716af5a4c719d40d2f7504fe9
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-02 12:59:35 +0530

    108

---
+++ Basic_JavaScript/108_usetheparseintfunction.js
@@ -0,0 +1,18 @@
+/*
+The parseInt() function parses a string and returns an integer.
+
+Here's an example:
+
+const a = parseInt("007");
+
+--> The above function converts the string 007 to the integer 7.
+--> If the first character in the string can't be converted into a number, then it returns NaN.
+*/
+
+
+function convertToInteger(str) {
+  const convert = parseInt(str);
+  return convert;
+}
+
+convertToInteger("56");
