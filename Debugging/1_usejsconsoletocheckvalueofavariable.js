41f0ed9b3b6960ced0e50b80e2f234706d492e61
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-10 12:05:05 +0530

    1

---
+++ Debugging/1_usejsconsoletocheckvalueofavariable.js
@@ -0,0 +1,20 @@
+/*
+The console.log() method, which "prints" the output of what's within its parentheses to the console, will likely be the most helpful debugging tool.
+
+--> Placing it at strategic points in your code can show you the intermediate values of variables.
+--> It's good practice to have an idea of what the output should be before looking at what it is.
+--> Having check points to see the status of your calculations throughout your code will help narrow down where the problem is.
+
+--> Here's an example to print the string Hello world! to the console:
+
+console.log('Hello world!');
+*/
+
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
