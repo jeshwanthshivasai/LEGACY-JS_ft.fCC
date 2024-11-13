587130fea50eaaf804ec23cbaed92a3ff9f6296c
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-11-13 20:50:54 +0530

    11

---
+++ Object Oriented Programming/11_iterateoverallproperties.js
@@ -0,0 +1,51 @@
+/*
+You have now seen two kinds of properties: own properties and prototype properties.
+
+--> Own properties are defined directly on the object instance itself.
+--> And prototype properties are defined on the prototype.
+
+function Bird(name) {
+  this.name = name;  //own property
+}
+Bird.prototype.numLegs = 2; // prototype property
+
+let duck = new Bird("Donald");
+
+--> Here is how you add duck's own properties to the array ownProps and prototype properties to the array prototypeProps:
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in duck) {
+  if(duck.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+console.log(ownProps);
+console.log(prototypeProps);
+
+--> console.log(ownProps) would display ["name"] in the console.
+--> And console.log(prototypeProps) would display ["numLegs"].
+*/
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+for(let property in beagle) {
+  if(beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
