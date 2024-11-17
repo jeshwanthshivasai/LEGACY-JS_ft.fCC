9fdc89a4951e72777ec7d23f149a547e2a0a7286
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-11-17 13:06:34 +0530

    21

---
+++ Object Oriented Programming/21_addmethodsafterinheritance.js
@@ -0,0 +1,48 @@
+/*
+A constructor function that inherits its prototype object from a supertype constructor function can still have its own methods in addition to inherited methods.
+
+For example, Bird is a constructor that inherits its prototype from Animal:
+
+function Animal() { }
+Animal.prototype.eat = function() {
+  console.log("nom nom nom");
+};
+
+function Bird() { }
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+
+--> In addition to what is inherited from Animal, you want to add behavior that is unique to Bird objects.
+--> Here, Bird will get a fly() function.
+--> Functions are added to Bird's prototype the same way as any constructor function:
+
+Bird.prototype.fly = function() {
+  console.log("I'm flying!");
+};
+Now instances of Bird will have both eat() and fly() methods:
+
+let duck = new Bird();
+duck.eat();
+duck.fly();
+
+--> duck.eat() would display the string nom nom nom in the console.
+--> duck.fly() would display the string I'm flying!.
+*/
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function() {
+  console.log("Woof!");
+}
+
+// Only change code above this line
+
+let beagle = new Dog();
+beagle.eat();
+beagle.bark();
